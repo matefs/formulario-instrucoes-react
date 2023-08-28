fe7a07ca09e46b2291684cd81fa651009abf86c4
matefs <matefs8569@gmail.com>
2023-08-27 22:38:49 -0300

    chore: removed unecessary text from parent component

--- src/App.js
+++ src/App.js
@@ -24,8 +24,6 @@ export default function App() {
 
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
       <LineInstructions
         lineInstructions={lineInstructions}
         setLineInstructions={setLineInstructions}
