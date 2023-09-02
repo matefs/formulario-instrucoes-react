689f7983476ae94f59198932d18609f4106b87c4
matefs <matefs8569@gmail.com>
2023-09-01 21:13:44 -0300

    chore: recebendo as instrucoes do componente pai via props

--- src/LineInstructions.jsx
+++ src/LineInstructions.jsx
@@ -1,25 +1,6 @@
 import React, { useState } from 'react';
 
-function Form() {
-  const [lineInstructions, setLineInstructions] = useState([
-    {
-      startLine: 1,
-      endLine: 1,
-      fields: [
-        { name: 'Company Name', startPos: 1, endPos: 27 },
-        { name: 'Fiscal Year', startPos: 28, endPos: 31 },
-      ],
-    },
-    {
-      startLine: 2,
-      endLine: 3,
-      fields: [
-        { name: 'Campo 1 ao 8', startPos: 1, endPos: 8 },
-        { name: 'Another Field', startPos: 8, endPos: 28 },
-      ],
-    },
-  ]);
-
+function Form({lineInstructions,setLineInstructions}) {
   const [startLine, setStart] = useState('');
   const [endLine, setEndLine] = useState('');
 
